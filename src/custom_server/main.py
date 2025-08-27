da619041b86d52f077551f89d77d73a00e79c7eb
Molony <matt.molony@databricks.com>
2025-08-27 19:45:41 +1000

    Enable Databricks configuration and restore catalog creation function in app.py; update main.py to run on port 8000

--- src/custom_server/main.py
+++ src/custom_server/main.py
@@ -6,6 +6,6 @@ def main():
     uvicorn.run(
         "custom_server.app:app",  # import path to your `app`
         host="0.0.0.0",
-        port=8002,
+        port=8000,
         reload=True,  # optional
     )
